aeb97858e4c05c5b954944f0697d89e4e8b6ad32
serkouh <bderrahmane1999@gmail.com>
2023-06-01 22:22:18 +0100

    Remove all files

--- src/main/java/org/sid/cinema/dao/SeanceRepository.java
+++
@@ -1,9 +0,0 @@
-package org.sid.cinema.dao;
-
-import org.sid.cinema.entities.Seance;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface SeanceRepository extends JpaRepository<Seance,Long> {
-}
